f70de22ff9709a9a4e6d26f4915716bc9fb3f52f
Praveen Kumar Krishnamoorthy <kpraveen15703@gmail.com>
2020-09-16 12:13:47 +0530

    Update get.groovy

--- vars/get.groovy
+++ vars/get.groovy
@@ -1,5 +1,5 @@
 def call(body) {
-def get = new URL("http://35.224.91.226:5000/todo/api/v1.0/tasks").openConnection();
+def get = new URL("http://localhost:5000/todo/api/v1.0/tasks").openConnection();
 def getRC = get.getResponseCode();
 println(getRC);
 if(getRC.equals(200)) {
